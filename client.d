1116b998c64037949383444632bb32ef9fe91bea
Tristan B. Kildaire <deavmi@disroot.org>
2017-10-05 20:15:55 +0200

    Add files via upload

--- client.d
+++ client.d
@@ -4,5 +4,10 @@ import std.socket;
 void main()
 {
 	writeln("Connecting...");
-	Socket conn = new Socket(parseAddress("127.0.0.1",25515));
+	AddressInfo addrInfo = AddressInfo();
+	addrInfo.protocol = ProtocolType.TCP;
+	addrInfo.family = AddressFamily.INET;
+	addrInfo.type = SocketType.STREAM;
+	Socket conn = new Socket(addrInfo);
+	conn.connect(parseAddress("127.0.0.1",25515));		
 }
